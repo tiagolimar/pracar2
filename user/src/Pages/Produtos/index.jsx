6591a995b1f409b4b73c63fc2ed67ed8fbb5c04a
tiagolimar <tiagolima.r2@gmail.com>
2024-02-06 19:22:20 -0300

    update user

--- user/src/Pages/Produtos/index.jsx
+++ user/src/Pages/Produtos/index.jsx
@@ -7,20 +7,30 @@ const Categoria = ({ nomeCategoria }) => {
     const [produtos, setProdutos] = useState([]);
     
     useEffect(() => {
-        fetch(`https://db-praca-r2.onrender.com/api/produto/categoria/${nomeCategoria}`)
+        console.log(`https://db-praca-r2.onrender.com/api/produtos/categoria/${nomeCategoria}`);
+        fetch(`https://db-praca-r2.onrender.com/api/produtos/categoria/${nomeCategoria}`)
             .then(res => res.json())
             .then(setProdutos)
             .catch(error => console.error('Erro ao carregar produtos:', error));
     }, [nomeCategoria]);
 
     return (
-        <div className="produtos row  row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
+        <div className="produtos row  row-cols-sm-2 row-cols-md-4 row-cols-lg-6 border-top border-3 border-dark bg-light shadow mt-4 rounded">
+            <h2 className='text-center'>{nomeCategoria}</h2>
             {produtos.map((produto, id) => (
                 <ItemProduto key={id} produto={produto} />
             ))}
         </div>
     );
 };
+
+const Banner = ()=>{
+    return(
+        <div className="banner d-flex align-items-center justify-content-center bg-dark text-white">
+        BANNER
+    </div>
+    )
+}
 
 export const Produtos = () => {
     const [categorias, setCategorias] = useState([]);
@@ -33,7 +43,8 @@ export const Produtos = () => {
     }, []);
 
     return (
-        <div>
+        <div className='mb-4'>
+            <Banner />
             {categorias.map((categoria,id) => (
                 <Categoria key={id} nomeCategoria={categoria} />
             ))}
