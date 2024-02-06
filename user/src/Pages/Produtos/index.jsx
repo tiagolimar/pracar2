1aca6ea782dde4303311c6eddfde1c954b6901c3
tiagolimar <tiagolima.r2@gmail.com>
2024-02-05 21:33:15 -0300

    update

--- user/src/Pages/Produtos/index.jsx
+++ user/src/Pages/Produtos/index.jsx
@@ -3,9 +3,9 @@ import './style.css'
 
 const ItemProduto = ({ produto }) => {
     return (
-        <div className="cartao-produto d-flex flex-column mt-3">
-            <img src={produto.img} alt={produto.nome} className='rounded-4 mb-2'/>
-            <header className='d-flex justify-content-between'>
+        <div className="cartao-produto d-flex flex-column mt-2">
+            <img src={produto.img} alt={produto.nome} className='rounded-4 shadow-sm mb-2'/>
+            <header className='d-flex justify-content-between gap-2'>
                 <h2>{produto.nome}</h2>
                 <p>R$ {produto.preco.toFixed(2)}</p>
             </header>
@@ -15,7 +15,8 @@ const ItemProduto = ({ produto }) => {
 };
 
 export const Produtos = () => {
-    const listaProdutos = [...Array(12)].map(() => ({
+    const listaProdutos = [...Array(12)].map((id) => ({
+        id: parseInt(`213${id}`),
         nome: "Bolo mole",
         descricao: "Uma fatia de bolo mole de aproximadamente  150g",
         categoria: "Bolo e Tapioca",
@@ -24,7 +25,7 @@ export const Produtos = () => {
     }));
 
     return (
-        <div className="produtos row-sm row-md row-lg row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
+        <div className="produtos row  row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
             {listaProdutos.map((produto, id) => (
                 <ItemProduto key={id} produto={produto} />
             ))}
