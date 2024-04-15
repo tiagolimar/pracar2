13e17ce805fb27f99f560e9c0eb4ee9be88e8e5d
tiagolimar <tiagolima.r2@gmail.com>
2024-04-15 13:14:48 -0300

    updadte

--- src/components/URLs/index.js
+++ src/components/URLs/index.js
@@ -1,5 +1,5 @@
-export const URL = "http://localhost:5000";
-// export const URL = "https://praca-r2-api.onrender.com";
+// export const URL = "http://localhost:5000";
+export const URL = "https://praca-r2-api.onrender.com";
 
 export const URL_BASE = `${URL}/api`;
 
